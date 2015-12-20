5e1fceec1eb4f24729f8b9b9945ac18801ef19ac
SergeevSW <sergeevsergeyw@gmail.com>
2015-12-21 01:46:33 +0300

    завершен 32ой урок

--- Gemfile
+++ Gemfile
@@ -2,3 +2,6 @@ source "https://rubygems.org"
 
 gem "sinatra"
 gem "sinatra-reloader"
+gem "sinatra-activerecord"
+gem 'activerecord'
+gem 'sqlite3'
--- app.rb
+++ app.rb
@@ -2,7 +2,19 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:pizzashop.db"
+
+class Product < ActiveRecord::Base
+  self.table_name = 'tProduct'
+end
+
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb :index
+end
+
+get '/about' do
+  erb :about
 end
